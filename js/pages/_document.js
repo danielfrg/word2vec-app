8d42d84939bdccf0996225e47bd6e891e7709d72
Daniel Rodriguez <daniel@danielfrg.com>
2021-09-19 18:57:47 -0500

    Switch to tailwind

--- js/pages/_document.js
+++ js/pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
 
     render() {
         return (
-            <Html html="en">
+            <Html lang="en">
                 <Head>
                     <meta charSet="utf-8" />
                     <meta
@@ -56,6 +56,7 @@ class MyDocument extends Document {
                         }}
                     />
                 </Head>
+
                 <body className="w-full h-full bg-orange">
                     <Main />
                     <NextScript />
